e971bd7a5e7c56954d3f584363fa8ce37a3eaf12
swk <swk@mseiot.com>
2022-05-17 00:47:13 +0800

    add file package

---
+++ file/main.go
@@ -0,0 +1,63 @@
+package file
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+func CopyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+	srcinfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
+}
+
+func CopyDir(src string, dst string) error {
+	srcFileInfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcFileInfo.Mode()); err != nil {
+		return err
+	}
+
+	fileInfos, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+	for _, fileInfo := range fileInfos {
+		srcfp := path.Join(src, fileInfo.Name())
+		dstfp := path.Join(dst, fileInfo.Name())
+		if fileInfo.IsDir() {
+			if err = CopyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = CopyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
